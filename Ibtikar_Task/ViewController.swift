5667e7571fddbff8ab1209142e42c18bdf85e2c9
Mohamed Samir <mohamedsamirsalem100@gmail.com>
2021-05-01 14:12:22 +0200

    Initial Commit

---
+++ Ibtikar_Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ibtikar_Task
+//
+//  Created by mohamed samir on 01/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
